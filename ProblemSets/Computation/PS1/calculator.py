e953e6de7f3a836ab36327d6611740337746c4cf
Daniel Dimitrov <daniel.k.dimitrov@gmail.com>
2019-07-16 22:47:58 -0500

    Commiting again p-s after I deleted them

---
+++ ProblemSets/Computation/PS1/calculator.py
@@ -0,0 +1,11 @@
+# calculator.py
+
+def my_sum(x,y):
+    z = x+y
+    return z
+
+def my_product(x,y):
+    z = x*y
+    return z
+
+from math import sqrt
---
+++ ProblemSets/Computation/PS1/object_oriented.py
@@ -0,0 +1,8 @@
+class ContentFilter:
+    def __init__(self, name):
+        self.name = name
+		
+	def fileRead(f_name):
+		open(f_name)
+		return f_name
+		
